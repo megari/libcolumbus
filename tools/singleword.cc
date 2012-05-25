d8b4bd0660c78a504d5e93d5046f8e2a2ec02c62
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2012-05-25 13:35:53 +0300

    Can delete window.

--- tools/singleword.cc
+++ tools/singleword.cc
@@ -20,13 +20,37 @@
 
 #include <gtk/gtk.h>
 
+static gboolean delete_event( GtkWidget *widget,
+                              GdkEvent  *event,
+                              gpointer   data )
+{
+    gtk_main_quit();
+    return TRUE;
+}
+
+/* Another callback */
+static void destroy(GtkWidget *widget,
+                     gpointer   data )
+{
+    gtk_main_quit ();
+}
+
+GtkWidget* build_gui() {
+    GtkWidget *window;
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    g_signal_connect (window, "delete-event",
+            G_CALLBACK (delete_event), NULL);
+    g_signal_connect (window, "destroy",
+            G_CALLBACK (destroy), NULL);
+    return window;
+}
+
 int main(int argc, char **argv) {
     GtkWidget *window;
     gtk_init(&argc, &argv);
 
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    window = build_gui();
     gtk_widget_show_all(window);
-
     gtk_main();
     return 0;
 }
