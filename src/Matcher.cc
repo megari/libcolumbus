c2268fca7ddd7e3c73fbb34213f0e00f70f10306
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2012-06-01 14:36:53 +0300

    Removed ResulGatherer class because it turned out useless.

--- src/Matcher.cc
+++ src/Matcher.cc
@@ -21,7 +21,6 @@
 #include "Document.hh"
 #include "WordList.hh"
 #include "IndexMatches.hh"
-#include "ResultGatherer.hh"
 #include "MatchResults.hh"
 #include <map>
 #include <set>
@@ -37,6 +36,70 @@ struct MatcherPrivate {
 typedef map<Word, LevenshteinIndex*>::iterator IndIterator;
 typedef map<Word, map<Word, set<const Document*> > >::iterator RevIndIterator;
 typedef map<Word, set<const Document*> >::iterator RevIterator;
+
+typedef map<Word, int> MatchErrorMap;
+
+struct ResultGathererPrivate {
+    // For each index list matched words and the smallest error with which they were matched.
+    map<Word, MatchErrorMap> bestIndexMatches;
+};
+
+typedef map<Word, MatchErrorMap>::iterator MatchIndIterator;
+typedef map<Word, int>::iterator MatchIterator;
+
+/*
+ * These are helper functions for Matcher. They are not member functions to avoid polluting the header
+ * with STL includes.
+ */
+
+static void addMatches(map<Word, MatchErrorMap> &bestIndexMatches, const Word &queryWord, const Word &indexName, IndexMatches &matches) {
+    MatchIndIterator it = bestIndexMatches.find(indexName);
+    map<Word, int> *indexMatches;
+    if(it == bestIndexMatches.end()) {
+        map<Word, int> tmp;
+        bestIndexMatches[indexName] = tmp;
+        it = bestIndexMatches.find(indexName);
+    }
+    indexMatches = &(it->second);
+    for(size_t i=0; i < matches.size(); i++) {
+        const Word &matchWord = matches.getMatch(i);
+        const int matchError = matches.getMatchError(i);
+        MatchIterator mIt = indexMatches->find(matchWord);
+        if(mIt == indexMatches->end()) {
+            (*indexMatches)[matchWord] = matchError;
+        } else {
+            if(mIt->second > matchError)
+                (*indexMatches)[matchWord] = matchError;
+        }
+
+    }
+}
+
+static double calculateRelevancy(const Word &w, int error) {
+    return 100.0/(100.0+error);
+}
+
+static void gatherMatchedDocuments(Matcher *matcher, map<Word, MatchErrorMap> &bestIndexMatches, map<const Document*, double> &matchedDocuments) {
+    for(MatchIndIterator it = bestIndexMatches.begin(); it != bestIndexMatches.end(); it++) {
+        for(MatchIterator mIt = it->second.begin(); mIt != it->second.end(); mIt++) {
+            vector<const Document*> tmp;
+            matcher->findDocuments(mIt->first, it->first, tmp);
+            debugMessage("Exact searched \"%s\" in field \"%s\", which was found in %ld documents.\n",
+                    mIt->first.asUtf8(), it->first.asUtf8(), tmp.size());
+            for(size_t i=0; i<tmp.size(); i++) {
+                const Document *curDoc = tmp[i];
+                // At this point we know the matched word, and which index and field
+                // it matched in. Now we can just increment the relevancy of said document.
+                double relevancy = calculateRelevancy(mIt->first, mIt->second);
+                map<const Document*, double>::iterator doc = matchedDocuments.find(curDoc);
+                if(doc == matchedDocuments.end())
+                    matchedDocuments[curDoc] = relevancy;
+                else
+                    matchedDocuments[curDoc] += relevancy;
+            }
+        }
+    }
+}
 
 Matcher::Matcher(Corpus *corp) {
     p = new MatcherPrivate();
@@ -102,7 +165,7 @@ void Matcher::addToReverseIndex(const Word &word, const Word &indexName, const D
 
 void Matcher::matchWithRelevancy(const WordList &query, const bool dynamicError, MatchResults &matchedDocuments) {
     map<const Document*, double> docs;
-    ResultGatherer r(this);
+    map<Word, MatchErrorMap> bestIndexMatches;
     for(size_t i=0; i<query.size(); i++) {
         const Word &w = query[i];
         int maxError;
@@ -113,13 +176,13 @@ void Matcher::matchWithRelevancy(const WordList &query, const bool dynamicError,
         for(IndIterator it = p->indexes.begin(); it != p->indexes.end(); it++) {
             IndexMatches m;
             it->second->findWords(w, maxError, m);
-            r.addMatches(w, it->first, m);
+            addMatches(bestIndexMatches, w, it->first, m);
             debugMessage("Matched word %s in index %s with error %d and got %ld matches.\n",
                     w.asUtf8(), it->first.asUtf8(), maxError, m.size());
         }
     }
     // Now we know all matched words in all indexes. Gather up the corresponding documents.
-    r.gatherMatchedDocuments(docs);
+    gatherMatchedDocuments(this, bestIndexMatches, docs);
     debugMessage("Found a total of %ld documents.\n", matchedDocuments.size());
     for(map<const Document*, double>::iterator it=docs.begin(); it != docs.end(); it++) {
         matchedDocuments.addResult(it->first->getID(), it->second);
