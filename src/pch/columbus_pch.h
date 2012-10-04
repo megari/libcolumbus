a2b5395beeb91d5e133a6035e9e7c92b2c26c986
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2012-10-04 15:16:16 +0300

    Added precompiled header support.

---
+++ src/pch/columbus_pch.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2012 Canonical, Ltd.
+ *
+ * Authors:
+ *    Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This is the precompiled header file for core libcolumbus.
+ * It contains only system headers. It must NOT have any internal
+ * headers
+ */
+
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+#include <stdexcept>
