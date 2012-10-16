8a2544f359c4e559925d4182c26bc523216e429b
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2012-10-16 11:17:31 +0300

    Started work on result filtering.

---
+++ src/ResultFilter.cc
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2012 Canonical, Ltd.
+ *
+ * Authors:
+ *    Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef USE_PCH
+#include "columbus_pch.hh"
+#else
+#include <vector>
+#endif
+
+#include "ResultFilter.hh"
+
+COL_NAMESPACE_START
+using namespace std;
+
+ResultFilter::ResultFilter() {
+
+}
+
+ResultFilter::~ResultFilter() {
+}
+
+COL_NAMESPACE_END
+
