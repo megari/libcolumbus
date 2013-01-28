728620556a088b466e1d1e9980c489c586b98a23
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-01-28 13:21:16 +0200

    Started working on memory efficien trie.

---
+++ include/Trie.hh
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * Authors:
+ *    Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU Lesser General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TRIE_HH
+#define TRIE_HH
+
+#include "ColumbusCore.hh"
+
+COL_NAMESPACE_START
+
+struct TriePrivate;
+
+class Trie {
+    TriePrivate *p;
+
+public:
+    Trie();
+    ~Trie();
+};
+
+COL_NAMESPACE_END
+
+#endif /*  */
---
+++ src/Trie.cc
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * Authors:
+ *    Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU Lesser General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include"Trie.hh"
+
+#include<sys/mman.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<stdio.h>
+#include<errno.h>
+#include<string.h>
+
+COL_NAMESPACE_START
+
+struct TriePrivate {
+    FILE *f;
+};
+
+Trie::Trie() {
+    p = new TriePrivate();
+    p->f = tmpfile();
+}
+
+Trie::~Trie() {
+    fclose(p->f);
+    delete p;
+}
+
+COL_NAMESPACE_END
