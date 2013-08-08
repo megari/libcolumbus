60ceddf5716722106f29ee06437f19d85f4c9692
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-08-08 14:06:36 +0300

    Detect Boost.Python manually, because CMake's Boost module is horribly broken.

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -1,14 +1,22 @@
 set(build_python FALSE)
 
-find_package(Boost 1.49.0 COMPONENTS python)
+set(Boost_DEBUG TRUE)
+
+# CMake's Boost.Python detector is completely and utterly
+# broken. We have to do this manually.
+#
+# Upstream bug:
+# http://public.kitware.com/Bug/view.php?id=12955
+find_file(BP_HEADER boost/python.hpp)
+
 if(use_python2)
   pkg_search_module(PYTHONLIBS python)
 else()
   pkg_search_module(PYTHONLIBS python3)
 endif()
 
-if(NOT Boost_FOUND)
-  message(STATUS "Boost not found, not building Python bindings.")
+if(NOT BP_HEADER)
+  message(STATUS "Boost.Python not found, not building Python bindings.")
 else()
   if(NOT PYTHONLIBS_FOUND)
     message(STATUS "Python dev libraries not found, not building Python bindings.")
@@ -19,11 +27,8 @@ else()
     if(NOT use_python2)
       execute_process(COMMAND ${CMAKE_SOURCE_DIR}/cmake/pysoabi.py OUTPUT_VARIABLE pysoabi OUTPUT_STRIP_TRAILING_WHITESPACE)
     endif()
-
-    # Linking against libboost_python does not work with Python 3.
-    # Working around this bug:
-    # http://public.kitware.com/Bug/view.php?id=12955
     find_library(BOOST_PYTHON_HACK boost_python-py${PYTHON_MAJOR}${PYTHON_MINOR})
+
     if(NOT BOOST_PYTHON_HACK)
       message(STATUS "Boost.Python hack library not found, not building Python bindings")
     else()
